b8fb43039de31db4830f30720787539e5a2703d3
Govindp51 <160322209+Govindp51@users.noreply.github.com>
2024-03-20 20:15:58 +0530

    Add files via upload
    Copy and past your VS code

---
+++ App.js
@@ -0,0 +1,39 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './Components/Login';
+import MainContainers from './Components/MainContainers';
+import Welcome from './Components/Welcome';
+import ChatArea from './Components/ChatArea';
+import CreateGroups from './Components/CreateGroups';
+import Users from './Components/Users';
+import Groups from './Components/Groups';
+// import ConversationItem from './Components/ConversationItem';
+import { useDispatch, useSelector } from 'react-redux';
+import Conversations from './Components/Conversations';
+//import UserGroup from './Components/UserGroup';
+
+function App() {
+  const dispatch = useDispatch();
+  const lightTheme = useSelector((state)=> state.themeKey);
+  return (
+    <div className={'Apps' + (lightTheme ? "" : " -dark")}>
+        {/* <MainContainers/> */}
+    {/* <Login/> */}
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+      <Route path="app" element={<MainContainers/>}>
+        <Route path="welcome" element={<Welcome/>}/>
+        <Route path="chat" element={<ChatArea/>}/>
+        <Route path="user-group" element={<Groups/>}/>
+        <Route path="users" element={<Users/>}/>
+        <Route path="create-group" element={<CreateGroups/>}/>
+        <Route path="conversations" element={<Conversations/>}/>
+
+      </Route>
+    </Routes>
+    </div>
+  
+  );
+}
+
+export default App;
